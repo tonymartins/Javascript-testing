c57174a5b170088c1c4c34bf07cf2e8bce8f3dfe
António Martins <antonio.martins6702@gmail.com>
2022-07-30 21:35:12 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let hello = true;
+let name = "Gabriela"; 
+
+if (hello === true) {
+    console.log ("Hello, " + name + "!");
+}
+    else {
+        console.log("Sorry there isn't anyone to greet.");
+    }
+
+var InputEvent {
+
+    
+}
